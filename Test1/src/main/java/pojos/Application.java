c98d0fadbb2bf69fc16beef96884c0f6b0c49d6b
SteefP <s.pelgrom@hotmail.com>
2017-11-16 09:49:15 +0100

    dgfsdfg

--- Test1/src/main/java/pojos/Application.java
+++ Test1/src/main/java/pojos/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("com.mycompany_hibernateBegin_jar_1.0-SNAPSHOTPU");
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Hibernate");
 		EntityManager em = entityManagerFactory.createEntityManager();
 
 		em.getTransaction().begin();
