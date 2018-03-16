7c5f39cd343aa7ea21917721f6e11479551bd7d9
SteefP <s.pelgrom@hotmail.com>
2018-03-16 17:51:50 +0100

    jklgjkl

--- Test1/src/main/java/p12/GenericDao.java
+++ Test1/src/main/java/p12/GenericDao.java
@@ -4,34 +4,76 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
 public class GenericDao<T> {
-	
-	EntityManager em;
-	
-	public GenericDao(EntityManager em) {
-		this.em = em;
+
+	private final Class<T> persistentClass;
+
+	EntityManagerFactory entityfactory;
+
+	public GenericDao(Class<T> persistentClass, EntityManagerFactory emf) {
+		this.persistentClass = persistentClass;
+		entityfactory = emf;
 	}
+
 
 
 
 
 
 	public void add(T pojo){
-		
-		
-		
-		
+		EntityManager em = entityfactory.createEntityManager();
+
+
+
 		em.getTransaction().begin();
 		em.persist(pojo);
 		em.getTransaction().commit();
 		em.clear();
-	
-	
+		em.close();
 
-	
-	
-		
+	}
+
+	public T read(int i){
+		EntityManager em = entityfactory.createEntityManager();
+		System.out.println("In read");
+		System.out.println(persistentClass);
+
+		T result = (T) em.find(persistentClass, i);
+		em.close();
+		return result;
+
+	}
+
+
+
+	public T readBykey(int id) {
+		EntityManager em = entityfactory.createEntityManager();
+
+		Session session = em.unwrap(Session.class);
+
+		Query query = session.createQuery("from " +persistentClass.getName()+" i Where i.id = " +id); 
+
+		T result = (T) query.getSingleResult();
+		em.close();
+		return result;
 	}
 	
-	
+	public T readByForeignkey(int id) {
+		EntityManager em = entityfactory.createEntityManager();
+
+		Session session = em.unwrap(Session.class);
+
+		Query query = session.createQuery("from " +persistentClass.getName()+" i Where i.hond1 = " +id); 
+
+		T result = (T) query.getSingleResult();
+		em.close();
+		return result;
+	}
+
+
 }
